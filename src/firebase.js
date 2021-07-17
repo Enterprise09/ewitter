ef221cee0238eab120916af060520f08c884d3d5
Enterprise09 <goldenstaytes@gmail.com>
2021-07-17 17:11:06 +0900

    Install Firebase SDK

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDu3hLlfvGQph98ZYb26l5zQ97MX2iGOr8",
+  authDomain: "ewitter-c0071.firebaseapp.com",
+  projectId: "ewitter-c0071",
+  storageBucket: "ewitter-c0071.appspot.com",
+  messagingSenderId: "412684830118",
+  appId: "1:412684830118:web:58e6c9ec525ab6fce97b67",
+};
+
+export default firebase.initializeApp(firebaseConfig);
