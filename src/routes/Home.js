6ccb7827042bcf223f574d30eace0cd4eac43f87
Enterprise09 <goldenstaytes@gmail.com>
2021-08-09 18:32:21 +0900

    clean code

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,10 @@
 import Eweet from "components/Eweet";
-import { dbService, storageService } from "fbase";
+import EweetFactory from "components/EweetsFactory";
+import { dbService } from "fbase";
 import React, { useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 const Home = ({ userObj }) => {
-  const [eweet, setEweet] = useState("");
   const [eweets, setEweets] = useState([]);
-  const [attachment, setAttachment] = useState("");
   useEffect(() => {
     dbService.collection("eweets").onSnapshot((snapshot) => {
       const eweetArray = snapshot.docs.map((doc) => ({
@@ -16,66 +14,9 @@ const Home = ({ userObj }) => {
       setEweets(eweetArray);
     });
   }, []);
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    let attachmentUrl = "";
-    if (attachment !== "") {
-      const attachmentRef = storageService
-        .ref()
-        .child(`${userObj.uid}/${uuidv4()}`);
-      const response = await attachmentRef.putString(attachment, "data_url");
-      attachmentUrl = await response.ref.getDownloadURL();
-    }
-    const eweetObj = {
-      text: eweet,
-      createAt: Date.now(),
-      creatorId: userObj.uid,
-      attachmentUrl,
-    };
-    await dbService.collection("eweets").add(eweetObj);
-    setEweet("");
-    setAttachment("");
-  };
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setEweet(value);
-  };
-  const onFileChange = (event) => {
-    const {
-      target: { files },
-    } = event;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-  const onClearAttachment = () => setAttachment(null);
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={eweet}
-          onChange={onChange}
-          type="text"
-          placeholder="What's on your mind"
-          maxLength={120}
-        />
-        <input type="file" accept="image/*" onChange={onFileChange} />
-        <input type="submit" value="EWeet" />
-        {attachment && (
-          <div>
-            <img src={attachment} width="50px" height="50px" />
-            <button onClick={onClearAttachment}>Clear</button>
-          </div>
-        )}
-      </form>
+      <EweetFactory userObj={userObj} />
       <div>
         {eweets.map((eweet) => (
           <Eweet
